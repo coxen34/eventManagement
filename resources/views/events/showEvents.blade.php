5a7850bb9bbb0f235b68a0ad98441fb19163b287
Rosa <xenco34@gmail.com>
2023-10-24 20:19:56 +0200

    confirmation message to delete event

--- resources/views/events/showEvents.blade.php
+++ resources/views/events/showEvents.blade.php
@@ -12,15 +12,36 @@
     </div>
 @endif
 
-    <ul>
-        @foreach ($events as $event)
-            <li>{{ $event->title }}</li>
-            <a href="{{ route('events.editField', ['eventId' => $event->id, 'field' => 'title']) }}">Editar</a>
-            <form method="POST" action="{{ route('events.destroy', ['eventId' => $event->id])}}">
-                @csrf
-                @method('DELETE')
-                <button type="submit" class="btn btn-danger">Eliminar</button>
-            </form>
-        @endforeach
-    </ul>
+<ul>
+    @foreach ($events as $event)
+        <li>{{ $event->title }}</li>
+        <a href="{{ route('events.editField', ['eventId' => $event->id, 'field' => 'title']) }}">Editar</a>
+        <form method="POST" action="{{ route('events.destroy', ['eventId' => $event->id])}}">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn btn-danger delete-event">Eliminar</button>
+        </form>
+    @endforeach
+</ul>
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            const deleteButtons = document.querySelectorAll('.delete-event');
+    
+            deleteButtons.forEach((button) => {
+                button.addEventListener('click', function (e) {
+                    if (!confirm('¿Estás seguro de que deseas eliminar el evento?')) {
+                        e.preventDefault(); // Cancela la acción de eliminación si el usuario cancela la confirmación.
+                    }
+                });
+            });
+        });
+    </script>
+    
+    
+    
+    
+    
+    
+    
 @endsection
